cd7623b4702dec6d5c2d0783547684b31cae4007
Gabriel <ghigared@gmail.com>
2023-07-15 10:30:00 -0600

    added first problem

--- 01_fib_practice/solution.js
+++ 01_fib_practice/solution.js
@@ -1,20 +1,22 @@
-
-const solution = (limit = 10) => {
-	let firstNumber = 0;
-	let secondNumber = 1;
-	let oddSum = 0;
-
-	while ((firstNumber) < limit) {
-		let tempSum = firstNumber + secondNumber;
-		firstNumber = secondNumber;
-		secondNumber = tempSum;
-
-		if (firstNumber % 2 != 0) {
-			oddSum += firstNumber;
-		}
+const reverseFibonacci = (n) => {
+	if (n < 1) return [];
+	
+	// Initialize the first two numbers of the sequence
+	let sequence = [0, 1];
+	
+	// Calculate the rest of the sequence up to n
+	while (sequence[sequence.length - 1] < n) {
+	  sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
 	}
-
-	return oddSum
+	
+	// Check if the last number is greater than n, if so remove it
+	if (sequence[sequence.length - 1] > n) {
+	  sequence.pop();
+	}
+  
+	// Reverse and print the sequence
+	console.log(sequence.reverse().join(', '));
 }
-
-console.log(solution(10000)) // 14328
+  
+// Call the function
+reverseFibonacci(5);
